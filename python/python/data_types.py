6c3d89410f7d79c5450e5bcd1777e960f1b28f64
juan stevan <alejosu245@gmail.com>
2026-02-24 10:27:16 -0500

    data_types

---
+++ python/python/data_types.py
@@ -0,0 +1,14 @@
+'''
+this is firts python script
+developer: Juan Stevan
+description: This script let us understand how to is data types.
+'''
+print("Hello everyone. Welcome to data types class")
+
+age = 17 
+print(age)
+print(type(age))
+EARTH_GRAVITY = 9.8
+PI =3.1416
+print(type(EARTH_GRAVITY))
+print(type(PI))
---
+++ python/variables.py
@@ -0,0 +1,5 @@
+first_name ="Stevan"
+print(type(first_name))
+print("your name is"firstname)
+print(f"your name is:{}")
+
