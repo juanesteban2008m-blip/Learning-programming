3ce828d0ebbb0fd92492b9edcc52abe4df5cec8e
Stevan <juanesteban2008m@gmail.com>
2026-03-10 10:08:11 -0500

    parcial algoritmia

---
+++ python/basic_calc_v2.py
@@ -0,0 +1,22 @@
+#Declare variables or constants 
+'''
+#In Python, it is not necessary or mandatory to declare
+variables or constants, but you can initialize them 
+'''
+#assign or initialize varibales or constants
+num1 = 0
+num2 = 0
+
+# Inputs 
+print("Please, enter first number:")
+num1 = input()
+print("Please, enter second number:")
+num2 =input()
+#processes
+add = num1+num2
+#subs = num1-num2
+#mult = num1*num2
+#div = num1/num2
+
+# Outputs
+print("The addition is: ")
